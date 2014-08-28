735a2176343146ddb5a68e7b2cf726ca4000c67b
drb1001 <pi_anyone@hotmail.com>
2014-08-28 15:42:54 +0200

    completed task 11

--- task11/task11.py
+++ task11/task11.py
@@ -1,24 +1,44 @@
 
 #  http://www.pythonchallenge.com/pc/return/5808.html
 
-from PIL import Image
+from PIL import Image, ImageOps
 
 myimage = Image.open('cave.jpg')
 
 mysize = myimage.size
+print mysize
 
-for i in range(0,mysize[0]):
-    mypixel = myimage.getpixel((i, midpoint))[1]
-    myarray.append(mypixel)
-    if i % 7 == 0:
-        mystring = mystring + chr(mypixel)
-
-
-im1 = Image.new("RGB", (640, 480), 'white')
-im2 = Image.new("RGB", (640, 480), 'white')
+im1 = Image.new("RGB", (640, 480), 'black')
+pix1 = im1.load()
+im2 = Image.new("RGB", (640, 480), 'black')
+pix2 = im2.load()
 
 
+for i in range(0, mysize[0]):
+    for j in range(0, mysize[1]):
+        mypixel = myimage.getpixel((i, j))
+        if (i+j) % 2 == 0: pix1[i,j] = mypixel
+        else: pix2[i,j] = mypixel
 
 
-# im1.save('output1.jpg')
-# im2.save('output2.jpg')
+im1 = ImageOps.invert(im1)
+im2 = ImageOps.invert(im2)
+
+
+# for i in range(0, mysize[0]):
+#     for j in range(0, mysize[1]):
+#         mypixel = myimage.getpixel((i, j))
+#         if (i+j) % 2 == 0:
+#             if i%2 == 0: pix1[i/2,j] = mypixel
+#             else: pix1[(i-1)/2,j] = mypixel
+#         else:
+#             if i%2 == 0: pix2[i/2,j] = mypixel
+#             else: pix2[(i-1)/2,j] = mypixel
+
+
+im1.save('output1_i.png')
+print "output1 saved"
+im2.save('output2_i.png')
+print "output2 saved"
+
+#  http://www.pythonchallenge.com/pc/return/evil.html
